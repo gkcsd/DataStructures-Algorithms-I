3f549cd1936df4cd15e7b7a853f1ddeeff41aa00
gkcsd <kaleg2025@gmail.com>
2021-06-20 13:02:05 +0530

    NaiveDivisorsOfNumber example added

---
+++ src/mathematics/NaiveDivisorsOfNumber.java
@@ -0,0 +1,20 @@
+package mathematics;
+
+import java.util.Scanner;
+
+//TC- O(n) & AS- O(1)
+public class NaiveDivisorsOfNumber {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        printDivisors(n);
+    }
+    static void printDivisors(int n) {
+        for(int i = 1; i <= n; i++) {
+            if(n%i == 0) {
+                System.out.print(i + " ");
+            }
+        }
+    }
+}
