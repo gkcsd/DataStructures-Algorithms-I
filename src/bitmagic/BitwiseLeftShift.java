b8fad35c4c9b111de842eef7c55dae74010d2623
gkcsd <kaleg2025@gmail.com>
2021-06-23 13:32:11 +0530

    BitwiseLeftShift example added

---
+++ src/bitmagic/BitwiseLeftShift.java
@@ -0,0 +1,12 @@
+package bitmagic;
+
+public class BitwiseLeftShift {
+    public static void main(String[] args) {
+        int x = 3;
+        System.out.println(x<<1);
+        System.out.println(x<<2);
+        int y = 4;
+        int z = (x<<y);
+        System.out.println(z);
+    }
+}
