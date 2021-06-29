db425a8d0e5e7d0b433cda254c66dc329f46b341
gkcsd <kaleg2025@gmail.com>
2021-06-29 20:14:47 +0530

    OptimizedPowerOfTwoSolutionTwo example added

---
+++ src/mathematics/OptimizedPowerOfTwoSolutionTwo.java
@@ -0,0 +1,16 @@
+package mathematics;
+
+import java.util.Scanner;
+
+//Tc- O(1) & AS- O(1)
+public class OptimizedPowerOfTwoSolutionTwo {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        boolean res = isPowerOfTwoSolTwo(n);
+        System.out.println(res);
+    }
+    static boolean isPowerOfTwoSolTwo(int n) {
+        return (n!=0) && ((n&(n-1)) == 0);
+    }
+}
