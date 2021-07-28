e1fa107c7da7c003b3690ba49c1c37937053b837
gkcsd <kaleg2025@gmail.com>
2021-07-28 20:19:27 +0530

    DirectRecursiveProgramTwo example added

---
+++ src/recursion/DirectRecursiveProgramTwo.java
@@ -0,0 +1,15 @@
+package recursion;
+
+
+public class DirectRecursiveProgramTwo {
+    public static void main(String[] args) {
+        fun1(5);
+    }
+    static void fun1(int n) {
+        if(n == 0) {
+            return;
+        }
+        System.out.println("Hello World!");
+        fun1(n-1);
+    }
+}
