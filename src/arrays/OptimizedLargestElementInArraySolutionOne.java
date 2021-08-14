269d74c8bd3a47ba86982c5fda5935db352e0b62
gkcsd <kaleg2025@gmail.com>
2021-08-14 14:58:50 +0530

    OptimizedLargestElementInArraySolutionOne example added

---
+++ src/arrays/OptimizedLargestElementInArraySolutionOne.java
@@ -0,0 +1,28 @@
+package arrays;
+
+import java.util.Scanner;
+
+//TC- O(n)
+public class OptimizedLargestElementInArraySolutionOne {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        for(int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+        int res = getLargest(arr);
+        System.out.println(res);
+    }
+    static int getLargest(int arr[]) {
+        int n = arr.length;
+        int res = 0;
+
+        for(int i = 0; i < n; i++) {
+            if(arr[i] > arr[res]) {
+                res = arr[i];
+            }
+        }
+        return res;
+    }
+}
