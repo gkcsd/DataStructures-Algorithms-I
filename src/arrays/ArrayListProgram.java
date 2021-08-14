d8bf9084365c2d538c189427fa315beed287404b
gkcsd <kaleg2025@gmail.com>
2021-08-14 13:48:50 +0530

    ArrayListProgram example added

---
+++ src/arrays/ArrayListProgram.java
@@ -0,0 +1,13 @@
+package arrays;
+
+import java.util.ArrayList;
+
+public class ArrayListProgram {
+    public static void main(String[] args) {
+        ArrayList<Integer> al = new ArrayList<>();
+        al.add(10);
+        al.add(5);
+        al.add(15);
+        System.out.println(al);
+    }
+}
