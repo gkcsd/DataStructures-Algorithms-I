f1f65c2cf1eec5472245ae01d4597f639ba3856a
gkcsd <kaleg2025@gmail.com>
2021-08-25 16:12:58 +0530

    StringExampleTwo example added

---
+++ src/string/StringExampleTwo.java
@@ -0,0 +1,20 @@
+package string;
+
+public class StringExampleTwo {
+    public static void main(String[] args) {
+        String s1 = "geeks";
+        String s2 = "geeks";
+
+        if(s1 == s2)
+            System.out.println("YES");
+        else
+            System.out.println("NO");
+
+        String s3 = new String("geeks");
+
+        if(s1 == s3)
+            System.out.println("YES");
+        else
+            System.out.println("No");
+    }
+}
