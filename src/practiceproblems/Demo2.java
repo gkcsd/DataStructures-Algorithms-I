dbb1e3618aa89010c9019ee32ae599a6e725cd10
gkcsd <kaleg2025@gmail.com>
2021-12-19 12:52:01 +0530

    Demo2 solution added

---
+++ src/practiceproblems/Demo2.java
@@ -0,0 +1,43 @@
+package practiceproblems;
+
+import java.util.Scanner;
+
+public class Demo2 {
+        public static void main(String args[]) {
+            Scanner sc = new Scanner(System.in);
+
+            String str = sc.next();
+            int k = sc.nextInt();
+            char res = encodedChar(str, k);
+            System.out.println(res);
+        }
+
+    static char encodedChar(String str,int k) {
+
+        String expand = "";
+        String temp = "";
+        int freq = 0;
+
+        for (int i=0; i < str.length();) {
+            temp = "";
+            freq = 0;
+
+            while (i < str.length() && str.charAt(i)>='a' && str.charAt(i)<='z') {
+                temp += str.charAt(i);
+                i++;
+            }
+
+            while (i < str.length() && str.charAt(i)>='1' && str.charAt(i)<='9') {
+                freq = freq*10 + str.charAt(i) - '0';
+                i++;
+            }
+
+            for (int j=1; j<=freq; j++) expand += temp;
+
+        }
+
+        if (freq==0) expand += temp;
+
+        return (expand.charAt(k-1));
+    }
+}
