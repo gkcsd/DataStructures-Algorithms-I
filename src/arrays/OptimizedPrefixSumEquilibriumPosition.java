684393946e7845c6a5cebf23b9f04e6f58996547
gkcsd <kaleg2025@gmail.com>
2021-12-22 20:56:28 +0530

    OptimizedPrefixSumEquilibriumPosition solution added

---
+++ src/arrays/OptimizedPrefixSumEquilibriumPosition.java
@@ -0,0 +1,37 @@
+package arrays;
+
+import java.util.Scanner;
+
+public class OptimizedPrefixSumEquilibriumPosition {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        for(int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+        boolean res = getResult(arr);
+        System.out.println(res);
+    }
+    static boolean getResult(int[] arr) {
+        int n = arr.length;
+        int sum = 0;
+
+        for(int i = 0; i < n; i++) {
+            sum += arr[i];
+        }
+
+        int lSum = 0;
+
+        for(int i = 0; i < n; i++) {
+            if(lSum == (sum-arr[i])) {
+                return true;
+            }
+
+            lSum += arr[i];
+            sum -= arr[i];
+        }
+
+        return false;
+    }
+}
