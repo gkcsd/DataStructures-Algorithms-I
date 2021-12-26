33f2a049511a6390520a14bf03447f3889384bc8
gkcsd <kaleg2025@gmail.com>
2021-12-26 21:15:55 +0530

    NaiveSearchInSortedRotatedArray example added

---
+++ src/searching/NaiveSearchInSortedRotatedArray.java
@@ -0,0 +1,30 @@
+package searching;
+
+import java.util.Scanner;
+
+//TC- O(n) & AS- O(1)
+public class NaiveSearchInSortedRotatedArray {
+    static int getSearch(int[] arr, int x) {
+        int n = arr.length;
+
+        for(int i = 0; i < n; i++) {
+            if(arr[i] == x) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        for(int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+        int x = sc.nextInt();
+
+        int res = getSearch(arr, x);
+        System.out.println(res);
+    }
+}
