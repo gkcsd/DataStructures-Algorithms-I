3ec1a698a27dda0605411bbab32236efd2baca50
gkcsd <kaleg2025@gmail.com>
2022-01-01 17:11:36 +0530

    BubbleSort example added

---
+++ src/sorting/BubbleSort.java
@@ -0,0 +1,32 @@
+package sorting;
+
+import java.util.Scanner;
+
+//TC- O(n*n) & AS- O(1)
+public class BubbleSort {
+    static void getResult(int[] arr) {
+        int n = arr.length;
+        for(int i = 0; i < n-1; i++) {
+            for(int j = 0; j < n-i-1; j++) {
+                if(arr[j] > arr[j+1]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j+1];
+                    arr[j+1] = temp;
+                }
+            }
+        }
+        for(int x : arr) {
+            System.out.print(x + " ");
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        for(int i = 0; i < n; i++) {
+            arr[i]= sc.nextInt();
+        }
+        getResult(arr);
+    }
+}
